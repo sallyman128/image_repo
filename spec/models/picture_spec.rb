edff5a58686f7305369491fb571f3abc48cd2747
Salmaan <salmaan.arif.ali@gmail.com>
2021-04-27 19:37:50 -0400

    write tests for a Picture model

---
+++ spec/models/picture_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+Rspec.describe Picture, type: :model do
+  picture = Picture.new(
+    name: "picture of a baby elephant",
+    image: ""
+  )
+
+  it 'has a valid image attribute' do
+    expect(picture).to be_valid
+  end
+
+  it 'is invalid if that image already exists' do 
+    duplicate_picture = Picture.new(
+      name: "Same picture of a baby elephant",
+      image: ""
+    )
+
+  end
+end
