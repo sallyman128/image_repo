c241c888acfafd66ea86634e08655ec0996d0bc8
Salmaan <salmaan.arif.ali@gmail.com>
2021-05-07 00:03:25 -0400

    add specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,16 +3,14 @@ require 'rails_helper'
 describe UsersController do
   describe "POST create" do
     it 'creates a new user' do
-      # expect{
-      #     post :create, params: { :coupon => { :coupon_code => "ASD123", :store => "Dean and Deluca" } }
-      #   }.to change(Coupon,:count).by(1)
+      expect{
+          post :create, params: { :user => { :username => "KingTrident", :password => "toupee" } }
+        }.to change(User,:count).by(1)
     end
 
     it 'redirects to the root index' do
-      # post :create, params: { :coupon => { :coupon_code => "ASD123", :store => "Dean and Deluca" } }
-      #   expect(response).to redirect_to Coupon.last
+      post :create, params: { :user => { :username => "KingTrident", :password => "toupee" } }
+      expect(response).to redirect_to images_path
     end
   end
 end
-
-# users can signup
--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'Route to view' do
+describe 'User features:' do
   before do 
     @user = User.create(username: "Spongebob", password: "squarepants")
     @image1 = @user.images.build
@@ -20,5 +20,21 @@ describe 'Route to view' do
   it 'has a show page' do
     visit user_path(@user)
     expect(page.status_code).to eq(200)
+  end
+
+  it 'lets user sign up' do
+    # user = User.new(username: "squidward", password: "clarinet")
+
+    visit new_user_path
+    fill_in 'username', with: "squidward"
+    fill_in 'password', with: user.password
+    click_on "Sign up"
+
+    expect().to change(User, :count).by(1)
+    # expect {
+      #     post :create, params: { image: { user_id: user.id , picture: blob_for('african-elephant-close-up.jpg') } }
+      #   }.to change(Image, :count).by(1)
+
+    # expect(page).to have_content(""@user.username)
   end
 end
